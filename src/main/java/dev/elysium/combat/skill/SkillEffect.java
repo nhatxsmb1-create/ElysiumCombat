abf573a7fb2e4fb62dd5c1d283fe5fa9ab8c3995
nhatxsmb1-create <nhatxsmb1@gmail.com>
2026-07-26 12:56:59 +0700

    Create SkillEffect.java

---
+++ src/main/java/dev/elysium/combat/skill/SkillEffect.java
@@ -0,0 +1,13 @@
+package dev.elysium.combat.skill;
+
+public enum SkillEffect {
+    DAMAGE_AOE,      // Gay sat thuong AoE xung quanh
+    DAMAGE_SINGLE,   // Gay sat thuong ke thu gan nhat
+    KNOCKBACK,       // Day lui ke thu + lam cham
+    BUFF_SELF,       // Buff ban than (potion effect)
+    DEBUFF_AOE,      // Debuff tat ca ke thu xung quanh
+    PROJECTILE,      // Phong cau lua
+    DASH,            // Lang xe ve phia truoc
+    FIRE_ARROW,      // Ban ten lua
+    ARROW_RAIN       // Mua ten AoE
+}
