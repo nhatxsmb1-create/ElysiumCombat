0a50063f1d6b49dd73a287a6e5a60dd470d831c9
nhatxsmb1-create <nhatxsmb1@gmail.com>
2026-07-26 12:55:29 +0700

    Create PlayerClass.java

---
+++ src/main/java/dev/elysium/combat/clazz/PlayerClass.java
@@ -0,0 +1,13 @@
+package dev.elysium.combat.clazz;
+
+/**
+ * Enum cac class co trong game.
+ * Them class moi: them vao day + them vao classes.yml + skills.yml
+ */
+public enum PlayerClass {
+    NONE,
+    WARRIOR,
+    MAGE,
+    ARCHER,
+    ROGUE
+}
