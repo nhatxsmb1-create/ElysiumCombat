a2dfd950cf297a152b323227e11b98493f7d020a
nhatxsmb1-create <nhatxsmb1@gmail.com>
2026-07-26 23:18:51 +0700

    Update SkillEffect.java

--- src/main/java/dev/elysium/combat/skill/SkillEffect.java
+++ src/main/java/dev/elysium/combat/skill/SkillEffect.java
@@ -1,13 +1,8 @@
 package dev.elysium.combat.skill;
 
 public enum SkillEffect {
-    DAMAGE_AOE,      // Gay sat thuong AoE xung quanh
-    DAMAGE_SINGLE,   // Gay sat thuong ke thu gan nhat
-    KNOCKBACK,       // Day lui ke thu + lam cham
-    BUFF_SELF,       // Buff ban than (potion effect)
-    DEBUFF_AOE,      // Debuff tat ca ke thu xung quanh
-    PROJECTILE,      // Phong cau lua
-    DASH,            // Lang xe ve phia truoc
-    FIRE_ARROW,      // Ban ten lua
-    ARROW_RAIN       // Mua ten AoE
+    BUFF_POTION,   // Buff ban than bang potion effect
+    RESTORE_MANA,  // Hoi phuc mana ngay lap tuc
+    HEAL,          // Hoi phuc HP + Regeneration
+    DASH           // Di chuyen nhanh ve phia truoc
 }
